b1b8a5688aef5a8f5f5c77be688a38bc9e291e37
Paris <paris@sourcelair.com>
2016-03-29 12:58:30 +0300

    Implement settings

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,23 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+  // Hey
+})
 
 .controller('SettingsCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+  // Set initial values
+  $('#settings-primary-key').val(
+    localStorage.getItem('settings.primaryKey') || ''
+  );
+  $('#settings-secondary-key').val(
+    localStorage.getItem('settings.secondaryKey') || ''
+  );
+
+  // Set listeners to update values
+  $('#settings-primary-key').on('keyup', function () {
+    localStorage.setItem('settings.primaryKey', $(this).val());
+  });
+  $('#settings-secondary-key').on('keyup', function () {
+    localStorage.setItem('settings.secondaryKey', $(this).val());
+  });
 });
