4aebd8b4f27d331f439d5f980182d1be27f3702f
Paris <paris@sourcelair.com>
2016-03-29 13:27:01 +0300

    First attempt to implement POST

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,41 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
-  // Hey
+  // Set up POST - Create resource request
+  $('#post-create-resource-button').on('click', function () {
+    var initialText = $(this).text();
+
+    $(this).attr('disabled', 'disabeld').text('POSTing request');
+
+    var body = {
+      'vehicleType': 'train',
+      'maxSpeed': 125,
+      'avgSpeed': 90,
+      'speedUnit': 'mph'
+    };
+
+    $.ajax({
+      url: 'https://nbgdemo.azure-api.net/echo/resource',
+      beforeSend: function(xhrObj){
+        // Set request headers for authentication and content type
+        xhrObj.setRequestHeader('Content-Type','application/json');
+        xhrObj.setRequestHeader(
+          'Ocp-Apim-Subscription-Key', localStorage.getItem('settings.primaryKey')
+        );
+      },
+      type: 'POST',
+      // Request body
+      data: body,
+    })
+    .done(function(data) {
+      $(this).text(initialText).removeAttr('disabled');
+      alert('success');
+    })
+    .fail(function(res) {
+      $(this).text(initialText).removeAttr('disabled');
+      alert('error');
+    });
+  });
 })
 
 .controller('SettingsCtrl', function($scope) {
